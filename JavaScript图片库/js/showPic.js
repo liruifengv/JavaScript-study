2b968cc486a3d55537c6bc2034bd8ad063ca1c3e
liruifengv <15227870916@163.com>
2017-02-12 14:10:38 +0800

    二次提交

--- JavaScript图片库/js/showPic.js
+++ JavaScript图片库/js/showPic.js
@@ -2,19 +2,48 @@
  * Created by æ…√Œ on 2017/2/11.
  */
 function showPic(whichPic){
+    if(!document.getElementById("placeholder")) return false;
     var source=whichPic.getAttribute("href");
     var placeholder=document.getElementById("placeholder")
+    if(placeholder.nodeName!="IMG")return false;
     placeholder.setAttribute("src",source);
-    var text=whichPic.getAttribute("title");
-    var description=document.getElementById("description");
-    description.firstChild.nodeValue=text;
-
-
-
+    if(document.getElementById("description")){
+        var text=whichPic.getAttribute("title") ? whichPic.getAttribute("title"): "";
+        var description=document.getElementById("description");
+        if(description.firstChild.nodeType===3){
+            description.firstChild.nodeValue=text;
+        }
+    }
+   return true;
 }
+/*
 function countBodyChildren(){
     var body_element=document.getElementsByTagName("body")[0];
     alert(body_element.nodeType);
 
 }
 window.onload=countBodyChildren;
+*/
+function prepareGallery(){
+    if(!document.getElementsByTagName) {
+        return false;
+    }
+    if(!document.getElementById){
+        return false;
+    }
+    if(!document.getElementById("imagegallery")){
+        return false;
+    }
+    var gallery=document.getElementById("imagegallery");
+    var links=gallery.getElementsByTagName("a");
+    for(var i=0;i<links.length;i++){
+        links[i].onclick=function(){
+            return showPic(this)?false:true;
+        }
+
+       // links[i].onkeypress=links[i].onclick;
+
+    }
+}
+window.onload=prepareGallery;
+
