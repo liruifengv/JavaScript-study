cca9c549586503dc330db3dcf662afa0c57398ec
liruifengv <15227870916@163.com>
2017-02-18 17:38:03 +0800

    图片库进化

--- JavaScript图片库/js/showPic.js
+++ JavaScript图片库/js/showPic.js
@@ -1,29 +1,71 @@
 /**
  * Created by 旧梦 on 2017/2/11.
  */
-function showPic(whichPic){
-    if(!document.getElementById("placeholder")) return false;
-    var source=whichPic.getAttribute("href");
-    var placeholder=document.getElementById("placeholder")
-    if(placeholder.nodeName!="IMG")return false;
-    placeholder.setAttribute("src",source);
-    if(document.getElementById("description")){
-        var text=whichPic.getAttribute("title") ? whichPic.getAttribute("title"): "";
-        var description=document.getElementById("description");
-        if(description.firstChild.nodeType===3){
-            description.firstChild.nodeValue=text;
+
+/********************************/
+/********共享onload事件**********/
+/*把需要用到的函数绑定的匿名函数*/
+/********************************/
+function addloadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
         }
     }
-   return true;
 }
-/*
-function countBodyChildren(){
-    var body_element=document.getElementsByTagName("body")[0];
-    alert(body_element.nodeType);
 
+
+/*********************************************/
+/*根据insertBefore函数创建一个insertAfter函数*/
+/**********在已有元素后插入一个元素***********/
+/*********************************************/
+function insertAfter(newElement,targetElement){
+    var parent=targetElement.parentNode;
+    if(parent.lastChild==targetElement){
+        parent.appendChild(newElement);
+    }else{
+        parent.insertBefore(newElement,targetElement.nextSibling);
+    }
 }
-window.onload=countBodyChildren;
-*/
+
+/********************************/
+/**创建一个img元素和一个p元素****/
+/***把它们插入到图片列表的后面***/
+/********************************/
+function preparePlaceholder(){
+    if(!document.createElement) return false;
+    if(!document.createTextNode) return false;
+    if(!document.getElementById) return false;
+    if(!document.getElementById("imageGallery")) return false;
+
+    var placeholder=document.createElement("img");
+    placeholder.setAttribute("id","placeholder");
+    placeholder.setAttribute("src","img/1.jpg");
+    placeholder.setAttribute("alt","my picture gallery")
+
+
+    var description=document.createElement("p");
+    description.setAttribute("id","description");
+
+    var desctext=document.createTextNode("Choose an image");
+
+    description.appendChild(desctext);
+
+    var gallery=document.getElementById("imageGallery");
+
+    insertAfter(placeholder,gallery);
+
+    insertAfter(description,placeholder);
+}
+
+/********************************/
+/*********遍历图片库链接*********/
+/****点击链接时调用showPic函数***/
+/********************************/
 function prepareGallery(){
     if(!document.getElementsByTagName) {
         return false;
@@ -31,19 +73,52 @@ function prepareGallery(){
     if(!document.getElementById){
         return false;
     }
-    if(!document.getElementById("imagegallery")){
+    if(!document.getElementById("imageGallery")){
         return false;
     }
-    var gallery=document.getElementById("imagegallery");
+    var gallery=document.getElementById("imageGallery");
     var links=gallery.getElementsByTagName("a");
     for(var i=0;i<links.length;i++){
         links[i].onclick=function(){
-            return showPic(this)?false:true;
+            return showPic(this);
         }
 
-       // links[i].onkeypress=links[i].onclick;
+         links[i].onkeypress=links[i].onclick;
 
     }
 }
-window.onload=prepareGallery;
+/********************************/
+/***把占位符图片切换成目标图片***/
+/********************************/
+function showPic(whichPic){
+    if(!document.getElementById("placeholder")) return true;
+    var source=whichPic.getAttribute("href");
+    var placeholder=document.getElementById("placeholder")
+    placeholder.setAttribute("src",source);
+    if(!document.getElementById("description")) return false;
+    if(whichPic.getAttribute("title")){
+        var text=whichPic.getAttribute("title");
+    }else{
+        var text="";
+    }
+        var description=document.getElementById("description");
+        if(description.firstChild.nodeType==3){
+            description.firstChild.nodeValue=text;
+        }
+    return false;
+}
+
+/********************************/
+/************绑定函数************/
+/********************************/
+addloadEvent(preparePlaceholder);
+addloadEvent(prepareGallery);
+
+/*
+function countBodyChildren(){
+    var body_element=document.getElementsByTagName("body")[0];
+    alert(body_element.nodeType);
+}
+window.onload=countBodyChildren;
+*/
 
